f78958051a835a48da2c82792814f65f3e867fde
Carolina mora <carolinamorahernandez@hotmail.com>
2023-05-18 04:23:13 -0400

    mejoras al hover y el titulo de las noticias

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -46,28 +46,28 @@ const Gallery = () => {
           {publications.map((publication) => (
             <div
               style={{ margin: '10' }}
-              className="cursor-pointer block max-h-100 rounded-2xl overflow-hidden border border-gray-200 mb-3 bg-white shadow-gray-200 shadow-xl duration-300 hover:shadow-xl hover:shadow-black/20"
+              className="cursor-pointer block max-h-100 rounded-2xl overflow-hidden border border-gray-200 mb-3 bg-gray shadow-gray-200 shadow-xl duration-300 hover:shadow-xl hover:shadow-black/40"
               key={publication.id}
               onClick={() => navigate(`/noticias/${publication.slug}`)}
             >
               <img
-                className="max-h-96 w-full h-full object-cover object-center rounded-t-lg  transition duration-300 ease-in-out hover:opacity-70"
+                className="max-h-96 w-full h-full object-cover object-center rounded-t-lg  transition duration-300 ease-in-out hover:opacity-50"
                 src={publication.images[0].url}
                 alt={publication.name}
               />
-              <div className="px-3 py-2 text-left">
-                <h1 className="text-2xl leading-[1.1] text-md">
+              <div className="px-5 py-2 text-left">
+                <h1 className="text-xl leading-[1.5] text-md">
                   {publication.name}
                 </h1>
                 <p className="card-date font-thin text-xs py-4">
                   Creado el {formatoFecha(publication.publicationDate)}
                 </p>
                 <p className={`text-[0.85rem] font-thin`}>
-                  {publication.finalContent.split(' ').slice(0, 20).join(' ') +
+                  {publication.finalContent.split(' ').slice(0, 12).join(' ') +
                     '...'}
                 </p>
               </div>
-              <div className="px-3 py-3">
+              <div className="px-5 py-4">
                 <p className="text-xs">@{publication.author}</p>
               </div>
             </div>
