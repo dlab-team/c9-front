747d6bef3e509a493bfdbd461982fdaacb860c7a
Sebastian Vidal Aedo <sebastian@intercode.cl>
2023-05-30 16:00:07 -0400

    se agrega pagina 404

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import AdminPublicationsNew from './pages/Admin/Publications/new';
 import Publication from './pages/Publication';
 import EditOne from './pages/Admin/Publications/Edit';
 import ViewOne from './pages/Admin/Publications/View';
+import NotFound from './pages/NotFound';
 
 import './App.css';
 
@@ -17,31 +18,71 @@ const App = () => {
     <div>
       <AuthContextProvider>
         <BrowserRouter>
-            <Routes>
-              <Route exact path="/" element={<Layout><Home /></Layout>} />
-              <Route exact path="/noticias/:slug" element={<Layout><Publication /></Layout>} />
-              <Route exact path="/acceso" element={<Admin/>} />
-              <Route
-                exact
-                path="admin/publications"
-                element={<Layout><AdminPublications /></Layout>}
-              />
-              <Route
-                exact
-                path="admin/publications/new"
-                element={<Layout><AdminPublicationsNew /></Layout>}
-              />
-              <Route
-                exact
-                path="admin/publications/edit/:slug"
-                element={<Layout><EditOne /></Layout>}
-              />
-              <Route
-                exact
-                path="admin/publications/:slug"
-                element={<Layout><ViewOne /></Layout>}
-              />
-            </Routes>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <Layout>
+                  <Home />
+                </Layout>
+              }
+            />
+            <Route
+              exact
+              path="/noticias/:slug"
+              element={
+                <Layout>
+                  <Publication />
+                </Layout>
+              }
+            />
+            <Route exact path="/acceso" element={<Admin />} />
+            <Route
+              exact
+              path="admin/publications"
+              element={
+                <Layout>
+                  <AdminPublications />
+                </Layout>
+              }
+            />
+            <Route
+              exact
+              path="admin/publications/new"
+              element={
+                <Layout>
+                  <AdminPublicationsNew />
+                </Layout>
+              }
+            />
+            <Route
+              exact
+              path="admin/publications/edit/:slug"
+              element={
+                <Layout>
+                  <EditOne />
+                </Layout>
+              }
+            />
+            <Route
+              exact
+              path="admin/publications/:slug"
+              element={
+                <Layout>
+                  <ViewOne />
+                </Layout>
+              }
+            />
+            <Route
+              path="*"
+              element={
+                <Layout>
+                  <NotFound />
+                </Layout>
+              }
+            />
+          </Routes>
         </BrowserRouter>
       </AuthContextProvider>
     </div>
