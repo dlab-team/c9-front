b2ad78986e7e9cbdf11e7c5182857b48e4355f9a
Carlos <carlos060500@gmail.com>
2023-06-03 11:54:15 -0400

    adding kodea logo to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,38 +4,46 @@ import LogoYellow from '../../assets/images/logo_innova_yellow.png';
 import LogoMercurio from '../../assets/images/logo-elmercurio.png';
 import LogoMicrosoft from '../../assets/images/logo-microsoft.png';
 import LogoAdl from '../../assets/images/logo-adl.png';
+import LogoKodea from '../../assets/images/logo-kodea.png';
 
 const Footer = () => {
   return (
-      <footer className="p-4 lg:text-left mt-8 bg-secondary">
-        <div className="flex justify-center items-center">
-          <img src={LogoYellow} alt="LogoYellow" className="h-24" />
-        </div>
-        <div className="flex justify-center my-3">
-          <Link to="">
-            <img
-              src={LogoMercurio}
-              alt="Logo Innovacion"
-              className="h-16 rounded-full border border-white border-1"
-            />
-          </Link>
-          <Link to="">
-            <img
-              src={LogoMicrosoft}
-              alt="Logo Microsoft"
-              className="h-16 mx-8 rounded-full border border-white border-1"
-            />
-          </Link>
-          <Link to="">
-            <img
-              src={LogoAdl}
-              alt="Logo ADL"
-              className="h-16 rounded-full border border-white border-1"
-            />
-          </Link>
-        </div>
-      </footer>
-    );
+    <footer className='p-4 lg:text-left mt-8 bg-secondary'>
+      <div className='flex justify-center items-center'>
+        <img src={LogoYellow} alt='LogoYellow' className='h-24' />
+      </div>
+      <div className='flex justify-around my-3'>
+        <Link to=''>
+          <img
+            src={LogoMercurio}
+            alt='Logo Innovacion'
+            className='h-16 rounded-lg border border-white border-1'
+          />
+        </Link>
+        <Link to=''>
+          <img
+            src={LogoMicrosoft}
+            alt='Logo Microsoft'
+            className='h-16 rounded-lg border border-white border-1'
+          />
+        </Link>
+        <Link to=''>
+          <img
+            src={LogoAdl}
+            alt='Logo ADL'
+            className='h-16 rounded-lg border border-white border-1'
+          />
+        </Link>
+        <Link to=''>
+          <img
+            src={LogoKodea}
+            alt='Logo de Kodea'
+            className='h-16 border rounded-lg border-white border-1 bg-white p-1'
+          />
+        </Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
