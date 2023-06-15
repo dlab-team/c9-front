16847164f594e9bce04e1e47dfb9f8a4839b60ca
Sebastian Vidal Aedo <sebastian@intercode.cl>
2023-06-15 19:10:40 -0400

    eliminacion de etiquetas inferiores y se agrega loading inicial en pub. individual

--- src/pages/Publication/index.jsx
+++ src/pages/Publication/index.jsx
@@ -1,22 +1,27 @@
-import React, { useEffect, useState, useRef } from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
-import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
-import "./index.css";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // Importa los estilos del carrusel
-import { Carousel } from "react-responsive-carousel"; // Importa el componente de carrusel
+import React, { useEffect, useState, useRef } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { faGlobeAmericas } from '@fortawesome/free-solid-svg-icons';
+import { faTag } from '@fortawesome/free-solid-svg-icons';
+import './index.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Importa los estilos del carrusel
+import { Carousel } from 'react-responsive-carousel'; // Importa el componente de carrusel
 
-import bgIzq from "../../assets/images/bg-izq.png";
-import bgDer from "../../assets/images/bg-der.png";
+import bgIzq from '../../assets/images/bg-izq.png';
+import bgDer from '../../assets/images/bg-der.png';
 
-import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+
+import { Spinner } from '../../components/UI';
 
 const Publication = () => {
   const [publication, setPublication] = useState();
   const [showButton, setShowButton] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const carouselRef = useRef(null);
   const { slug } = useParams();
@@ -31,14 +36,14 @@ const Publication = () => {
   const handleScrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   };
 
   function formatFecha(publicationDate) {
     const fecha = new Date(publicationDate);
     const numeroDia = fecha.getDate();
-    const nombreMes = fecha.toLocaleString("es-ES", { month: "long" });
+    const nombreMes = fecha.toLocaleString('es-ES', { month: 'long' });
 
     return (
       <div className="bg-gray-200 text-center py-4 md:p-5 rounded">
@@ -71,9 +76,10 @@ const Publication = () => {
       setPublication(publication);
 
       if (carouselRef.current) {
-        const dots = document.querySelectorAll(".control-dots .dot");
+        const dots = document.querySelectorAll('.control-dots .dot');
         dots[0].click();
       }
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
@@ -91,161 +97,145 @@ const Publication = () => {
       setShowButton(isVisible);
     };
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
 
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
   useEffect(() => {
-    const divContent = document.getElementById("content-text");
+    const divContent = document.getElementById('content-text');
     divContent.innerHTML = publication?.finalContent;
   }, [publication]);
 
   return (
     <>
-      <div className="container container-flex mx-auto py-7">
-        <Link to="/">
-          <button
-            type="button"
-            className="btn_back text-blue-800 text-sm py-2.5 text-center inline-flex items-center"
-          >
-            <FontAwesomeIcon icon={faChevronLeft} className="mr-2" />
-            Volver
-          </button>
-        </Link>
-        <h1 className="innova-title pt-5">{publication?.name}</h1>
-        <div className="mt-2 flex justify-between">
-          <div>
-            {" "}
-            <FontAwesomeIcon
-              icon={faCircleUser}
-              className="pe-2 text-gray-500"
-            />
-            {publication?.author}
-          </div>
-
-          {/* Etiquetas de publicaciones  */}
-
-          <div className="flex gap-1 mr-4">
-            <a
-              href="/"
-              className=" inline-block whitespace-nowrap rounded-full bg-warning-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.85em] font-bold leading-none text-warning-800 hover:shadow-lg ease-in-out hover:scale-110"
-            >
-              {publication?.region}
-            </a>
-            <a
-              href="/"
-              className="inline-block whitespace-nowrap rounded-full bg-info-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.85em] font-bold leading-none text-info-800 hover:shadow-lg ease-in-out hover:scale-110"
-            >
-              {publication?.city}
-            </a>
-            <a
-              href="/"
-              className="inline-block whitespace-nowrap rounded-full bg-neutral-50 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.85em] font-bold leading-none text-neutral-600 hover:shadow-lg ease-in-out hover:scale-110"
-            >
-              {publication?.category ? publication.category : "Sin categoría"}
-            </a>
-          </div>
+      <div className={loading ? '' : 'hidden'}>
+        <div className="w-full h-[20vh] sm:h-[60vh] flex justify-center items-center">
+          <Spinner />
         </div>
       </div>
-      <Carousel
-        ref={carouselRef}
-        showStatus={false}
-        showThumbs={false}
-        centerMode
-        centerSlidePercentage={60}
-        autoPlay={true} // Se le agrega autoplay al carrusel
-        interval={5000} // se le define un intervalo
-        infiniteLoop
-        className="pb-4 mt-0 w-full"
-      >
-        {publication?.images.map((img, index) => (
-          <div key={`img_${index}`}>
-            <img
-              className="imgSlide w-full"
-              src={img.url}
-              alt={`Imagen ${index}`}
-            />
-          </div>
-        ))}
-      </Carousel>
-
-      <div className="publication-content relative">
-        <div className="absolute right-0 w-[40%] md:w-52 lg:w-[22%]">
-          <img src={bgDer} alt="Blob Derecho" />
-        </div>
-
-        <div className="absolute left-0 bottom-0 w-[50%] md:w-72 lg:w-[25%]">
-          <img src={bgIzq} alt="Blob Izquierdo" />
-        </div>
-
-        <div className="relative container mx-auto w-5/5 lg:w-4/5 py-4 whitespace-wrap">
-          <div className="grid grid-cols-8 gap-4">
-            <div className="col-span-1">
-              {formatFecha(publication?.publicationDate)}
+      <div className={loading ? 'hidden' : ''}>
+        <div className="container container-flex mx-auto py-7">
+          <Link to="/">
+            <button
+              type="button"
+              className="btn_back text-blue-800 text-sm py-2.5 text-center inline-flex items-center"
+            >
+              <FontAwesomeIcon icon={faChevronLeft} className="mr-2" />
+              Volver
+            </button>
+          </Link>
+          <h1 className="innova-title pt-5">{publication?.name}</h1>
+          <div className="mt-2 flex justify-between">
+            <div className="inline-block whitespace-nowrap rounded-full bg-secondary px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.85em] font-bold leading-none text-white hover:shadow-lg ease-in-out hover:scale-110">
+              {' '}
+              <FontAwesomeIcon
+                icon={faCircleUser}
+                className="pe-2 text-white "
+              />
+              {publication?.author}
             </div>
-            <div className="col-span-7">
-              <div id="content-text" className="innova-text"></div>
+
+            {/* Etiquetas de publicaciones  */}
+
+            <div className="flex gap-1 mr-4">
+              <a
+                href="/"
+                className="inline-block whitespace-nowrap rounded-full bg-neutral-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.85em] font-bold leading-none text-warning-800 hover:shadow-lg ease-in-out hover:scale-110"
+              >
+                <FontAwesomeIcon
+                  icon={faGlobeAmericas}
+                  className="pe-2 text-gray-500 "
+                />
+                {publication?.region} / {publication?.city}
+              </a>
+              <a
+                href="/"
+                className="inline-block whitespace-nowrap rounded-full bg-green-50 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.85em] font-bold leading-none text-neutral-600 hover:shadow-lg ease-in-out hover:scale-110"
+              >
+                <FontAwesomeIcon icon={faTag} className="pe-2 text-gray-500 " />
+                {publication?.category ? publication.category : 'Sin categoría'}
+              </a>
             </div>
           </div>
-          {/*  Etiquetas Publications */}
-
-          <div className="flex justify-end gap-1 my-2 mx-2  ">
-            <a
-              href="/"
-              className=" inline-block whitespace-nowrap rounded-full bg-warning-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.85em] font-bold leading-none text-warning-800 hover:shadow-lg ease-in-out hover:scale-110"
-            >
-              Región
-            </a>
-            <a
-              href="/"
-              className="inline-block whitespace-nowrap rounded-full bg-info-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.85em] font-bold leading-none text-info-800 hover:shadow-lg ease-in-out hover:scale-110"
-            >
-              Comuna
-            </a>
-            <a
-              href="/"
-              className="inline-block whitespace-nowrap rounded-full bg-neutral-50 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.85em] font-bold leading-none text-neutral-600 hover:shadow-lg ease-in-out hover:scale-110"
-            >
-              Categoría
-            </a>
-          </div>
         </div>
+        <Carousel
+          ref={carouselRef}
+          showStatus={false}
+          showThumbs={false}
+          centerMode
+          centerSlidePercentage={60}
+          autoPlay={true} // Se le agrega autoplay al carrusel
+          interval={5000} // se le define un intervalo
+          infiniteLoop
+          className="pb-4 mt-0 w-full"
+        >
+          {publication?.images.map((img, index) => (
+            <div key={`img_${index}`}>
+              <img
+                className="imgSlide w-full"
+                src={img.url}
+                alt={`Imagen ${index}`}
+              />
+            </div>
+          ))}
+        </Carousel>
 
-        <div className="relative innova-text container w-5/5 lg:w-4/5 mx-auto py-4">
-          <h2 className="pt-8">PREGUNTAS RELACIONADAS</h2>
-          <div className="innova-text">
-            {publication?.questions.map((item, index) => (
-              <div key={index} className="p-2">
-                <div
-                  className="bg-blue-100 flex justify-between items-center cursor-pointer p-3 rounded-lg"
-                  onClick={() => toggleQuestion(index)}
-                >
-                  <div className="p-2">{item.question}</div>
-                  <svg
-                    className={`w-5 h-5 transition-transform ${
-                      activeIndex === index ? "transform rotate-180" : ""
-                    }`}
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M19 9l-7 7-7-7" />
-                  </svg>
-                </div>
-                {activeIndex === index && (
-                  <div className="p-4 bg-gray-100 bg-opacity-60 rounded mt-2">
-                    {item.answer}
-                  </div>
-                )}
+        <div className="publication-content relative">
+          <div className="absolute right-0 w-[40%] md:w-52 lg:w-[22%]">
+            <img src={bgDer} alt="Blob Derecho" />
+          </div>
+
+          <div className="absolute left-0 bottom-0 w-[50%] md:w-72 lg:w-[25%]">
+            <img src={bgIzq} alt="Blob Izquierdo" />
+          </div>
+
+          <div className="relative container mx-auto w-5/5 lg:w-4/5 py-4 whitespace-wrap">
+            <div className="grid grid-cols-8 gap-4">
+              <div className="col-span-1">
+                {formatFecha(publication?.publicationDate)}
               </div>
-            ))}
+              <div className="col-span-7">
+                <div id="content-text" className="innova-text"></div>
+              </div>
+            </div>
+          </div>
+
+          <div className="relative innova-text container w-5/5 lg:w-4/5 mx-auto py-4">
+            <h2 className="pt-8">PREGUNTAS RELACIONADAS</h2>
+            <div className="innova-text">
+              {publication?.questions.map((item, index) => (
+                <div key={index} className="p-2">
+                  <div
+                    className="bg-blue-100 flex justify-between items-center cursor-pointer p-3 rounded-lg"
+                    onClick={() => toggleQuestion(index)}
+                  >
+                    <div className="p-2">{item.question}</div>
+                    <svg
+                      className={`w-5 h-5 transition-transform ${
+                        activeIndex === index ? 'transform rotate-180' : ''
+                      }`}
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M19 9l-7 7-7-7" />
+                    </svg>
+                  </div>
+                  {activeIndex === index && (
+                    <div className="p-4 bg-gray-100 bg-opacity-60 rounded mt-2">
+                      {item.answer}
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
