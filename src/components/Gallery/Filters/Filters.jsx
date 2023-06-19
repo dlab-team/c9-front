8178a63f64889cbff4fd904eb3251f746299af9a
Seba <sebastian_@live.com.au>
2023-06-19 12:09:00 -0400

    las etiquetas consumen data del back

--- src/components/Gallery/Filters/Filters.jsx
+++ src/components/Gallery/Filters/Filters.jsx
@@ -1,454 +1,30 @@
-import { useState, useEffect, useRef } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilter } from '@fortawesome/free-solid-svg-icons';
-import { Select, initTE } from 'tw-elements';
+import { useState, useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import { Select, initTE } from "tw-elements";
+import axios from "axios";
 
 // TODO: traer esta data desde el back
-const regiones = [
-  {
-    region: 'Metropolitana de Santiago',
-    comunas: [
-      'Cerrillos',
-      'Cerro Navia',
-      'Conchalí',
-      'El Bosque',
-      'Estación Central',
-      'Huechuraba',
-      'Independencia',
-      'La Cisterna',
-      'La Florida',
-      'La Granja',
-      'La Pintana',
-      'La Reina',
-      'Las Condes',
-      'Lo Barnechea',
-      'Lo Espejo',
-      'Lo Prado',
-      'Macul',
-      'Maipú',
-      'Ñuñoa',
-      'Pedro Aguirre Cerda',
-      'Peñalolén',
-      'Providencia',
-      'Pudahuel',
-      'Quilicura',
-      'Quinta Normal',
-      'Recoleta',
-      'Renca',
-      'Santiago',
-      'San Joaquín',
-      'San Miguel',
-      'San Ramón',
-      'Vitacura',
-      'Puente Alto',
-      'Pirque',
-      'San José de Maipo',
-      'Colina',
-      'Lampa',
-      'Tiltil',
-      'San Bernardo',
-      'Buin',
-      'Calera de Tango',
-      'Paine',
-      'Melipilla',
-      'Alhué',
-      'Curacaví',
-      'María Pinto',
-      'San Pedro',
-      'Talagante',
-      'El Monte',
-      'Isla de Maipo',
-      'Padre Hurtado',
-      'Peñaflor',
-    ],
-  },
-  {
-    region: 'Arica y Parinacota',
-    comunas: ['Arica', 'Camarones', 'Putre', 'General Lagos'],
-  },
-  {
-    region: 'Tarapacá',
-    comunas: [
-      'Iquique',
-      'Alto Hospicio',
-      'Pozo Almonte',
-      'Camiña',
-      'Colchane',
-      'Huara',
-      'Pica',
-    ],
-  },
-  {
-    region: 'Antofagasta',
-    comunas: [
-      'Antofagasta',
-      'Mejillones',
-      'Sierra Gorda',
-      'Taltal',
-      'Calama',
-      'Ollagüe',
-      'San Pedro de Atacama',
-      'Tocopilla',
-      'María Elena',
-    ],
-  },
-  {
-    region: 'Atacama',
-    comunas: [
-      'Copiapó',
-      'Caldera',
-      'Tierra Amarilla',
-      'Chañaral',
-      'Diego de Almagro',
-      'Vallenar',
-      'Alto del Carmen',
-      'Freirina',
-      'Huasco',
-    ],
-  },
-  {
-    region: 'Coquimbo',
-    comunas: [
-      'La Serena',
-      'Coquimbo',
-      'Andacollo',
-      'La Higuera',
-      'Paiguano',
-      'Vicuña',
-      'Illapel',
-      'Canela',
-      'Los Vilos',
-      'Salamanca',
-      'Ovalle',
-      'Combarbalá',
-      'Monte Patria',
-      'Punitaqui',
-      'Río Hurtado',
-    ],
-  },
-  {
-    region: 'Valparaíso',
-    comunas: [
-      'Valparaíso',
-      'Casablanca',
-      'Concón',
-      'Juan Fernández',
-      'Puchuncaví',
-      'Quintero',
-      'Viña del Mar',
-      'Isla de Pascua',
-      'Los Andes',
-      'Calle Larga',
-      'Rinconada',
-      'San Esteban',
-      'La Ligua',
-      'Cabildo',
-      'Papudo',
-      'Petorca',
-      'Zapallar',
-      'Quillota',
-      'Calera',
-      'Hijuelas',
-      'La Cruz',
-      'Nogales',
-      'San Antonio',
-      'Algarrobo',
-      'Cartagena',
-      'El Quisco',
-      'El Tabo',
-      'Santo Domingo',
-      'San Felipe',
-      'Catemu',
-      'Llaillay',
-      'Panquehue',
-      'Putaendo',
-      'Santa María',
-      'Quilpué',
-      'Limache',
-      'Olmué',
-      'Villa Alemana',
-    ],
-  },
-  {
-    region: 'Libertador Gral. Bernardo O’Higgins',
-    comunas: [
-      'Rancagua',
-      'Codegua',
-      'Coinco',
-      'Coltauco',
-      'Doñihue',
-      'Graneros',
-      'Las Cabras',
-      'Machalí',
-      'Malloa',
-      'Mostazal',
-      'Olivar',
-      'Peumo',
-      'Pichidegua',
-      'Quinta de Tilcoco',
-      'Rengo',
-      'Requínoa',
-      'San Vicente',
-      'Pichilemu',
-      'La Estrella',
-      'Litueche',
-      'Marchihue',
-      'Navidad',
-      'Paredones',
-      'San Fernando',
-      'Chépica',
-      'Chimbarongo',
-      'Lolol',
-      'Nancagua',
-      'Palmilla',
-      'Peralillo',
-      'Placilla',
-      'Pumanque',
-      'Santa Cruz',
-    ],
-  },
-  {
-    region: 'Maule',
-    comunas: [
-      'Talca',
-      'Constitución',
-      'Curepto',
-      'Empedrado',
-      'Maule',
-      'Pelarco',
-      'Pencahue',
-      'Río Claro',
-      'San Clemente',
-      'San Rafael',
-      'Cauquenes',
-      'Chanco',
-      'Pelluhue',
-      'Curicó',
-      'Hualañé',
-      'Licantén',
-      'Molina',
-      'Rauco',
-      'Romeral',
-      'Sagrada Familia',
-      'Teno',
-      'Vichuquén',
-      'Linares',
-      'Colbún',
-      'Longaví',
-      'Parral',
-      'Retiro',
-      'San Javier',
-      'Villa Alegre',
-      'Yerbas Buenas',
-    ],
-  },
-  {
-    region: 'Ñuble',
-    comunas: [
-      'Cobquecura',
-      'Coelemu',
-      'Ninhue',
-      'Portezuelo',
-      'Quirihue',
-      'Ránquil',
-      'Treguaco',
-      'Bulnes',
-      'Chillán Viejo',
-      'Chillán',
-      'El Carmen',
-      'Pemuco',
-      'Pinto',
-      'Quillón',
-      'San Ignacio',
-      'Yungay',
-      'Coihueco',
-      'Ñiquén',
-      'San Carlos',
-      'San Fabián',
-      'San Nicolás',
-    ],
-  },
-  {
-    region: 'Biobío',
-    comunas: [
-      'Concepción',
-      'Coronel',
-      'Chiguayante',
-      'Florida',
-      'Hualqui',
-      'Lota',
-      'Penco',
-      'San Pedro de la Paz',
-      'Santa Juana',
-      'Talcahuano',
-      'Tomé',
-      'Hualpén',
-      'Lebu',
-      'Arauco',
-      'Cañete',
-      'Contulmo',
-      'Curanilahue',
-      'Los Álamos',
-      'Tirúa',
-      'Los Ángeles',
-      'Antuco',
-      'Cabrero',
-      'Laja',
-      'Mulchén',
-      'Nacimiento',
-      'Negrete',
-      'Quilaco',
-      'Quilleco',
-      'San Rosendo',
-      'Santa Bárbara',
-      'Tucapel',
-      'Yumbel',
-      'Alto Biobío',
-    ],
-  },
-  {
-    region: 'La Araucanía',
-    comunas: [
-      'Temuco',
-      'Carahue',
-      'Cunco',
-      'Curarrehue',
-      'Freire',
-      'Galvarino',
-      'Gorbea',
-      'Lautaro',
-      'Loncoche',
-      'Melipeuco',
-      'Nueva Imperial',
-      'Padre las Casas',
-      'Perquenco',
-      'Pitrufquén',
-      'Pucón',
-      'Saavedra',
-      'Teodoro Schmidt',
-      'Toltén',
-      'Vilcún',
-      'Villarrica',
-      'Cholchol',
-      'Angol',
-      'Collipulli',
-      'Curacautín',
-      'Ercilla',
-      'Lonquimay',
-      'Los Sauces',
-      'Lumaco',
-      'Purén',
-      'Renaico',
-      'Traiguén',
-      'Victoria',
-    ],
-  },
-  {
-    region: 'Los Ríos',
-    comunas: [
-      'Valdivia',
-      'Corral',
-      'Lanco',
-      'Los Lagos',
-      'Máfil',
-      'Mariquina',
-      'Paillaco',
-      'Panguipulli',
-      'La Unión',
-      'Futrono',
-      'Lago Ranco',
-      'Río Bueno',
-    ],
-  },
-  {
-    region: 'Los Lagos',
-    comunas: [
-      'Puerto Montt',
-      'Calbuco',
-      'Cochamó',
-      'Fresia',
-      'Frutillar',
-      'Los Muermos',
-      'Llanquihue',
-      'Maullín',
-      'Puerto Varas',
-      'Castro',
-      'Ancud',
-      'Chonchi',
-      'Curaco de Vélez',
-      'Dalcahue',
-      'Puqueldón',
-      'Queilén',
-      'Quellón',
-      'Quemchi',
-      'Quinchao',
-      'Osorno',
-      'Puerto Octay',
-      'Purranque',
-      'Puyehue',
-      'Río Negro',
-      'San Juan de la Costa',
-      'San Pablo',
-      'Chaitén',
-      'Futaleufú',
-      'Hualaihué',
-      'Palena',
-    ],
-  },
-  {
-    region: 'Aisén del Gral. Carlos Ibáñez del Campo',
-    comunas: [
-      'Coihaique',
-      'Lago Verde',
-      'Aisén',
-      'Cisnes',
-      'Guaitecas',
-      'Cochrane',
-      'O’Higgins',
-      'Tortel',
-      'Chile Chico',
-      'Río Ibáñez',
-    ],
-  },
-  {
-    region: 'Magallanes y de la Antártica Chilena',
-    comunas: [
-      'Punta Arenas',
-      'Laguna Blanca',
-      'Río Verde',
-      'San Gregorio',
-      'Cabo de Hornos (Ex Navarino)',
-      'Antártica',
-      'Porvenir',
-      'Primavera',
-      'Timaukel',
-      'Natales',
-      'Torres del Paine',
-    ],
-  },
-];
 
-const categorias = [
-  { name: 'General' },
-  { name: 'Tecnología' },
-  { name: 'Ciencia' },
-  { name: 'Entretenimiento' },
-  { name: 'Espacio' },
-];
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 const Filters = () => {
   const [isRegionOpen, setIsRegionOpen] = useState(false);
   const [isComunaOpen, setIsComunaOpen] = useState(false);
   const [isCategoriaOpen, setIsCategoriaOpen] = useState(false);
+  const [categorias, setCategorias] = useState([]);
+  const [regiones, setRegiones] = useState([]);
 
   const regionDropdownRef = useRef(null);
   const comunaDropdownRef = useRef(null);
   const categoriaDropdownRef = useRef(null);
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
@@ -485,18 +61,31 @@ const Filters = () => {
     setIsCategoriaOpen(!isCategoriaOpen);
   };
 
-  const [currentRegion, setCurrentRegion] = useState(
-    'Metropolitana de Santiago'
-  );
-  const [currentComunas, setCurrentComunas] = useState('');
+  const [currentRegion, setCurrentRegion] = useState("Todas");
+  const [currentComunas, setCurrentComunas] = useState("");
 
-  const loadComunas = () => {
-    const index = regiones.findIndex((item) => item.region === currentRegion);
-    const comunas = regiones[index].comunas;
-    setCurrentComunas(comunas);
+  const getCategories = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_BACKEND_URL}/categories`
+    );
+    const data = await response.json();
+    const dataCapitalized = data.map((item) => {
+      item.name = capitalize(item.name);
+      return item;
+    });
+    setCategorias(dataCapitalized);
+  };
+  const getRegiones = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_BACKEND_URL}/regions`
+    );
+    const data = await response.json();
+    setRegiones(data);
   };
 
   useEffect(() => {
+    getCategories();
+    getRegiones();
     initTE({ Select });
   }, []);
 
@@ -504,10 +93,14 @@ const Filters = () => {
     loadComunas();
   }, [currentRegion]);
 
+  const loadComunas = () => {
+    const index = regiones.findIndex((region) => region.name === currentRegion);
+    const comunas = regiones[index]?.cities;
+    setCurrentComunas(comunas);
+  };
   return (
-    
-      <div className="mb-[2em] p-0 sm:gap-4 container flex flex-wrap justify-center md:justify-end mt-3 md:mr-0 md:gap-x-5 md:my-4 md:bg-white py-5">
-        <div className='flex mt-2 gap-2'>
+    <div className="mb-[2em] p-0 sm:gap-4 container flex flex-wrap justify-center md:justify-end mt-3 md:mr-0 md:gap-x-5 md:my-4 md:bg-white py-5">
+      <div className="flex mt-2 gap-2">
         <button type="button" className="sm:hidden flex items-center mr-2">
           <FontAwesomeIcon icon={faFilter} className="text-secondary h-6" />
         </button>
@@ -524,9 +117,10 @@ const Filters = () => {
                 setCurrentRegion(e.target.value);
               }}
             >
-              {regiones.map((item, index) => (
-                <option key={index} value={item.name}>
-                  {item.region}
+              <option value={"todas"}>Todas</option>
+              {regiones.map((item) => (
+                <option key={item.id} value={item.name}>
+                  {item.name}
                 </option>
               ))}
             </select>
@@ -539,10 +133,11 @@ const Filters = () => {
           data-te-dropdown-ref
         >
           <select data-te-select-init data-te-select-filter="true">
+            <option value={"todas"}>Todas</option>
             {currentComunas &&
-              currentComunas.map((item, index) => (
-                <option key={index} value={index}>
-                  {item}
+              currentComunas.map((item, id) => (
+                <option key={id} value={id}>
+                  {item.name}
                 </option>
               ))}
           </select>
@@ -554,8 +149,9 @@ const Filters = () => {
           data-te-dropdown-ref
         >
           <select data-te-select-init data-te-select-filter="true">
-            {categorias.map((item, index) => (
-              <option key={index} value={index}>
+            <option value={"todas"}>Todas</option>
+            {categorias.map((item) => (
+              <option key={item.id} value={item.id}>
                 {item.name}
               </option>
             ))}
