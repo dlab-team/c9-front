af11c966d6d1e68e1ba2ebc61ca19806dbde505b
Sebastian Vidal Aedo <sebastian@intercode.cl>
2023-06-28 11:14:06 -0400

    se agrega busqueda en responsivo

--- src/context/SearchContext/SearchContext.js
+++ src/context/SearchContext/SearchContext.js
@@ -1,16 +1,21 @@
-import { createContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { createContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const SearchContext = createContext();
 
 function SearchContextProvider(props) {
   const navigate = useNavigate();
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleSearch = (event) => {
-    if (event.keyCode === 13 && event.target.value.length > 0) {
-      setSearchTerm(event.target.value);
-      navigate(`/busqueda/${event.target.value}`);
+    if (event.keyCode === 13) {
+      if (event.target.value.length === 0) {
+        setSearchTerm('');
+        navigate(`/`);
+      } else {
+        setSearchTerm(event.target.value);
+        navigate(`/busqueda/${event.target.value}`);
+      }
     }
   };
 
