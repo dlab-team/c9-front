f3a16b11d06fc6001d83e91add14d8610358499d
Sebastian Vidal Aedo <sebastian@intercode.cl>
2023-06-28 16:53:53 -0400

    se agrega margen en footer responsive y se fixea compartir por email

--- src/components/Sharedbar/Sharedbar.jsx
+++ src/components/Sharedbar/Sharedbar.jsx
@@ -14,61 +14,61 @@ const Sharedbar = ({ description }) => {
   const url = window.location.href;
 
   return (
-    <div className='fixed right-0 top-1/2 transform -translate-y-1/2 mr-2'>
-      <div className='flex gap-1 flex-col items-center'>
+    <div className="fixed right-0 top-1/2 transform -translate-y-1/2 mr-2">
+      <div className="flex gap-1 flex-col items-center">
         {showIcons ? (
           <button
             onClick={toggleIcons}
-            className='p-1 rounded-full bg-gray-500 text-white w-12 h-12 flex items-center justify-center shadow-md hover:opacity-75'
+            className="p-1 rounded-full bg-gray-500 text-white w-12 h-12 flex items-center justify-center shadow-md hover:opacity-75"
           >
             <FontAwesomeIcon icon={faClose} />
           </button>
         ) : (
           <button
             onClick={toggleIcons}
-            className='p-1 rounded-full bg-secondary text-white w-12 h-12 flex items-center justify-center shadow-md hover:opacity-75'
+            className="p-1 rounded-full bg-secondary text-white w-12 h-12 flex items-center justify-center shadow-md hover:opacity-75"
           >
             <FontAwesomeIcon icon={faShareAlt} />
           </button>
         )}
 
         {showIcons && (
-          <div className='flex flex-col space-y-1'>
+          <div className="flex flex-col space-y-1">
             {/* Facebook */}
             <a
               href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}
-              target='_blank'
-              className='p-1 rounded-full bg-blue-500 text-white w-12 h-12 flex items-center justify-center shadow-md hover:opacity-75'
+              target="_blank"
+              className="p-1 rounded-full bg-blue-500 text-white w-12 h-12 flex items-center justify-center shadow-md hover:opacity-75"
             >
-              <FontAwesomeIcon icon={faFacebookF} className='text-3xl' />
+              <FontAwesomeIcon icon={faFacebookF} className="text-3xl" />
             </a>
             {/* Email  */}
             <a
-              href={`mailto:info@example.com?&subject= Tienes que ver esto!&cc=&bcc=&body=Inspirate con nuestro contenido:${url}\n${encodeURI(
+              href={`mailto:?&subject= Tienes que ver esto!&cc=&bcc=&body=${url}%0D%0A%0D%0A${encodeURI(
                 description
               )}`}
-              target='_blank'
-              className='p-1 my-1 rounded-full bg-red-500 text-white w-12 h-12 flex items-center justify-center shadow-md hover:opacity-75'
+              target="_blank"
+              className="p-1 my-1 rounded-full bg-red-500 text-white w-12 h-12 flex items-center justify-center shadow-md hover:opacity-75"
             >
-              <FontAwesomeIcon icon={faEnvelope} className='text-3xl' />
+              <FontAwesomeIcon icon={faEnvelope} className="text-3xl" />
             </a>
             {/* Whatsapp */}
             <a
               href={`https://api.whatsapp.com/send/?text=${url}&type=custom_url&app_absent=0`}
-              target='_blank'
-              className='p-1 rounded-full bg-green-500 text-white w-12 h-12 flex items-center justify-center shadow-md hover:opacity-75'
+              target="_blank"
+              className="p-1 rounded-full bg-green-500 text-white w-12 h-12 flex items-center justify-center shadow-md hover:opacity-75"
             >
-              <FontAwesomeIcon icon={faWhatsapp} className='text-3xl' />
+              <FontAwesomeIcon icon={faWhatsapp} className="text-3xl" />
             </a>
             {/* Twitter  */}
             <a
               href={`https://twitter.com/intent/tweet?url=${url}&text=${encodeURI(
                 description
               )}`}
-              className='p-1 rounded-full bg-blue-500 text-white w-12 h-12 flex items-center justify-center shadow-md hover:opacity-75'
-              target='_blank'
+              className="p-1 rounded-full bg-blue-500 text-white w-12 h-12 flex items-center justify-center shadow-md hover:opacity-75"
+              target="_blank"
             >
-              <FontAwesomeIcon icon={faTwitter} className='text-3xl' />
+              <FontAwesomeIcon icon={faTwitter} className="text-3xl" />
             </a>
           </div>
         )}
