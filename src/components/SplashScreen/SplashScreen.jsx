b6c72e9fcabb21613491cd3b969de6615a53ef68
Carolina mora <carolinamorahernandez@hotmail.com>
2023-07-26 03:08:58 -0400

    Acotar la aparición de la splash screen solo al recargar el home

--- src/components/SplashScreen/SplashScreen.jsx
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -2,21 +2,22 @@ import React, { useEffect, useState } from 'react';
 import logoInnova from '../../assets/images/logo_innova_yellow.png';
 
 
-const SplashScreen = () => {
+const SplashScreen = ({ onComplete }) => {
     const [showLogo, setShowLogo] = useState(true);
 
     useEffect(() => {
         // Ocultar el logo después de 1.5 segundos
         const timer = setTimeout(() => {
         setShowLogo(false);
+        onComplete(); // Llamada a la función onComplete cuando termine la animación
         }, 2000);
 
         // Limpiar el timer cuando el componente se desmonte
         return () => clearTimeout(timer);
-    }, []);
+    }, [onComplete]);
 
     return (
-        <div className={`flex items-center justify-center h-screen bg-blue-500 ${showLogo ? 'animate-slideUp' : ''}`}>
+        <div className={`fixed top-0 left-0 w-full h-full bg-blue-500 flex items-center justify-center z-50 ${showLogo ? 'animate-slideUp' : 'hidden'}`}>
         {showLogo && (
             <img
             src={logoInnova}
