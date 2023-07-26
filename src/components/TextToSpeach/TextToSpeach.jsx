104ff60285b107aea26cc0bbdbc34a2e266f87ac
Javier Figueroa <javiernfigueroa@gmail.com>
2023-07-26 13:37:18 -0400

    emojis

--- src/components/TextToSpeach/TextToSpeach.jsx
+++ src/components/TextToSpeach/TextToSpeach.jsx
@@ -61,7 +61,10 @@ const TextToSpeech = ({ text }) => {
     if (!text) {
       return "";
     }
-    const cleaned = text.replace(/<[^>]+>/g, "");
+    const cleaned = text.replace(
+      /<[^>]+>|[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F900}-\u{1F9FF}\u{1F1E0}-\u{1F1FF}]/gu,
+      ""
+    );
     return cleaned;
   };
 
