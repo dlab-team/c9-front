6bc7fc2c5cd77f599a1f4f0179a6f4543568fb9f
Daniel Atik <danielatik@users.noreply.github.com>
2024-01-19 22:09:35 -0300

    Revert "Update Form.jsx"
    This reverts commit b49258d3b287fe9c56b17abe042bb0badbe1acf0.

--- src/components/Publications/Form/Form.jsx
+++ src/components/Publications/Form/Form.jsx
@@ -393,7 +393,7 @@ const Form = ({ publication } = null) => {
     try {
       const response = await fetch(
         // 'https://api.openai.com/v1/chat/completions',
-        `${process.env.REACT_APP_BACKEND_URL}/turbo`,
+        `${process.env.REACT_APP_BACKEND_URL}/davinci`,
         optionsQA
       );
       const data = await response.json();
@@ -478,7 +478,7 @@ const Form = ({ publication } = null) => {
     try {
       const response = await fetch(
         // 'https://api.openai.com/v1/chat/completions',
-        `${process.env.REACT_APP_BACKEND_URL}/turbo`,
+        `${process.env.REACT_APP_BACKEND_URL}/davinci`,
         options
       );
 
@@ -567,7 +567,7 @@ const Form = ({ publication } = null) => {
     try {
       const response = await fetch(
         // 'https://api.openai.com/v1/chat/completions',
-        `${process.env.REACT_APP_BACKEND_URL}/turbo`,
+        `${process.env.REACT_APP_BACKEND_URL}/davinci`,
         options
       );
       const data = await response.json();
