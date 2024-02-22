57bf966fe298473b0229954d28626097e638a14b
Daniel Atik <danielatik@users.noreply.github.com>
2024-02-21 22:18:02 -0300

    ..

--- src/components/Publications/Form/Form.jsx
+++ src/components/Publications/Form/Form.jsx
@@ -534,26 +534,25 @@ const Form = ({ publication } = null) => {
         Authorization: `Bearer ${API_KEY}`,
         'Content-Type': 'application/json',
       },
+      // body: JSON.stringify({
+      //   model: 'gpt-4-0125-preview',
+      //   temperature: 0,
+      //   n: 1,
+      //   messages: [
+      //     {
+      //       role: 'user',
+      //       content: `Traduce en ingles el siguiente texto manteniendo las etiquetas HTML, estilos, emojis y saltos de lineas,
+      //   texto: '''${translatedText}'''`,
+      //     },
+      //   ],
+      // }),
       body: JSON.stringify({
         model: 'gpt-4-0125-preview',
         temperature: 0,
         n: 1,
-        messages: [
-          {
-            role: 'user',
-            content: `Traduce en ingles el siguiente texto manteniendo las etiquetas HTML, estilos, emojis y saltos de lineas,
-        texto: '''${translatedText}'''`,
-          },
-        ],
+        message: `Traduce en ingles el siguiente texto manteniendo las etiquetas HTML, estilos, emojis y saltos de lineas,
+      //   texto: '''${translatedText}'''`,
       }),
-      // body: JSON.stringify({
-      //   model: 'gpt-4-0125-preview',
-      //   temperature: 0,
-      //   max_tokens: 4096,
-      //   n: 1,
-      //   message: `Traduce en ingles el siguiente texto manteniendo las etiquetas HTML, estilos, emojis y saltos de lineas,
-      // //   texto: '''${translatedText}'''`,
-      // }),
     };
 
     try {
